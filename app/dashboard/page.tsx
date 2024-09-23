bbefcf88a4f79d0b3bf7a5b7a3b38c779f725a9f
tnploy <63070078@kmitl.ac.th>
2024-09-24 06:46:42 +0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+
+
 import {
   fetchRevenue,
   fetchLatestInvoices,
